575a63aea3141d16a55e00924ac97cde811cd6fc
Marcelo Santos <cbo19956@gmail.com>
2022-07-12 22:43:06 +0100

    Initial committ

---
+++ README.md
@@ -0,0 +1,21 @@
+# Scoreboard Display
+
+### Screenshot
+
+![](images/print.png)
+
+## My process
+
+Hi, this is a solo and a very simple example of a typical score display used in games such as Basketball, Hockey and many others.
+
+### How the project works
+
+So, this is basicallly a score display, where there is a Home team and a Guest team.
+We can input the wanted values such as +1 +2 and +3 into the display Scoreboard.
+We can also reset the scoreboard as whenever we wish to do so.
+
+### Built with
+
+- Semantic HTML5
+- CSS
+- Javascript
---
+++ Solo_Project_BasketScoreChart/index.js
@@ -0,0 +1,58 @@
+let homeScore = 0;
+let homeScorePoint = document.getElementById('home_score');
+
+let guestScore = 0;
+let guestScorePoint = document.getElementById('guest_score');
+
+//Reset button
+let resetButton = document.getElementById('btn-reset');
+
+// homeScore function
+
+function addOneHome() {
+  let newAddPoint = (homeScore += 1);
+  homeScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+function addTwoHome() {
+  let newAddPoint = (homeScore += 2);
+  homeScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+function addThreeHome() {
+  let newAddPoint = (homeScore += 3);
+  homeScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+//guestScore function
+
+function addOneGuest() {
+  let newAddPoint = (guestScore += 1);
+  guestScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+function addTwoGuest() {
+  let newAddPoint = (guestScore += 2);
+  guestScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+function addThreeGuest() {
+  let newAddPoint = (guestScore += 3);
+  guestScorePoint.textContent = newAddPoint;
+  console.log(newAddPoint);
+}
+
+//reset score function
+
+function reset() {
+  homeScore = 0;
+  guestScore = 0;
+  newAddPoint = 0;
+  homeScorePoint.textContent = 0;
+  guestScorePoint.textContent = 0;
+}
