b8e90769a180c138c78353e32786e99e82538d79
Marcelo Santos <cbo19956@gmail.com>
2022-07-14 22:28:47 +0100

    super optmized functions

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-let homeScore = 0;
-let homeScorePoint = document.getElementById('home_score');
-
-let guestScore = 0;
-let guestScorePoint = document.getElementById('guest_score');
-
-let scoreBoard = 0;
-//Reset button
-let resetButton = document.getElementById('btn-reset');
-
-let addPoint = 0;
-let numAdd = 0;
 // homeScore function
 
 function choose(teamToAdd, addPoint) {
-  if (teamToAdd == homeScorePoint) {
-    homeScore = add(homeScore, addPoint);
-    homeScorePoint.textContent = homeScore;
-  } else {
-    guestScore = add(guestScore, addPoint);
-    guestScorePoint.textContent = guestScore;
-  }
-}
-
-function add(scoreBoard, numAdd) {
-  scoreBoard += numAdd;
-  return scoreBoard;
+  let element = document.getElementById(teamToAdd);
+  element.textContent = parseInt(element.textContent) + addPoint;
 }
 
 function reset() {
-  homeScore = 0;
-  guestScore = 0;
-  homeScorePoint.textContent = 0;
-  guestScorePoint.textContent = 0;
+  document.getElementById('home_score').textContent = 0;
+  document.getElementById('guest_score').textContent = 0;
 }
